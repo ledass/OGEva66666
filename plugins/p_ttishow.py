e090681dcf2fcfe73e4d10e2f55f7e3cc4406fee
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-06-06 19:05:42 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -170,18 +170,23 @@ async def stats_command(bot, message):
 async def gen_invite(bot, message):
     if len(message.command) == 1:
         return await message.reply('Give me a chat id')
+
     chat = message.command[1]
+
     try:
         chat = int(chat)
     except:
         return await message.reply('Give Me A Valid Chat ID')
+
     try:
         link = await bot.create_chat_invite_link(chat)
     except ChatAdminRequired:
-        return await message.reply("Invite Link Generation Failed, Iam Not Having Sufficient Rights")
+        return await message.reply("Invite Link Generation Failed, I am not having sufficient rights")
     except Exception as e:
-        return await message.reply(f'Error {e}')
-    await message.reply(f'Here is your Invite Link {link.invite_link}')
+        return await message.reply(f'Error: {e}')
+
+    await message.reply(f'Here is your Invite Link: {link.invite_link}')
+
 
 @Client.on_message(filters.command('ban') & filters.user(ADMINS))
 async def ban_a_user(bot, message):
