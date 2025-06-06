aab26da3167daa8e114a9e53b21613d730b4331b
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-06-06 19:23:06 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -12,7 +12,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings,humanbytes
 from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_search_results
+from database.ia_filterdb import Media2, Media3, Media4, get_file_details, get_search_results
 from database.filters_mdb import (
     del_all,
     find_filter,
