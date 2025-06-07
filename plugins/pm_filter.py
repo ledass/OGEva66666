1ba57b07f11a1027a05a0e7bb0cb40ece79f8fa2
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2025-06-07 07:08:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -306,7 +306,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
-        f_caption = files.caption
+        f_caption = getattr(files, "caption", None)
         settings = await get_settings(query.message.chat.id)
         if CUSTOM_FILE_CAPTION:
             try:
